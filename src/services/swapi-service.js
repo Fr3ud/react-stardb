632926fb77989a54fd6d0876980f6f1678c13119
Fr3ud <oo.pavelko@gmail.com>
2019-01-12 15:58:26 +0200

    Add base structure

---
+++ src/services/swapi-service.js
@@ -0,0 +1,56 @@
+export default class SwapiService {
+
+    _apiBase = 'https://swapi.co/api';
+
+    async getResource(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, received ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    async getAllPeople() {
+        const res = await this.getResource(`/people/`);
+
+        return res.results;
+    }
+
+    getPerson(id) {
+        return this.getResource(`/people/${id}`);
+    }
+
+    async getAllPlanets() {
+        const res = await this.getResource(`/planets/`);
+
+        return res.results;
+    }
+
+    getPlanet(id) {
+        return this.getResource(`/planets/${id}`);
+    }
+
+    async getAllStarships() {
+        const res = await this.getResource(`/starships/`);
+
+        return res.results;
+    }
+
+    getStarship(id) {
+        return this.getResource(`/starships/${id}`);
+    }
+}
+
+
+// const swapi = new SwapiService();
+//
+// swapi.getAllPeople().then((body) => console.log(body));
+// swapi.getPerson(1).then((body) => console.log(body));
+//
+// swapi.getAllPlanets().then((body) => console.log(body));
+// swapi.getPlanet(2).then((body) => console.log(body));
+//
+// swapi.getAllStarships().then((body) => console.log(body));
+// swapi.getStarship(3).then((body) => console.log(body));
